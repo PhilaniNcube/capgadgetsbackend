1924358f835bc2da359219221b964fb7d0f16777
Philani Ncube <ncbphi001@gmail.com>
2022-01-25 19:31:30 +0200

    orders content type

--- config/server.js
+++ config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '1972df44ecf4b54754494d83f338c822'),
-    },
-  },
+      secret: env("ADMIN_JWT_SECRET", "1972df44ecf4b54754494d83f338c822")
+    }
+  }
 });
